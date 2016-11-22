ac480dfb82d2b11e6cc48cff1c7d6f60c4773582
Frances Maxwell <fmaxwell@gmail.com>
2016-11-22 15:11:37 +0000

    Post model created. Posts can be created. Wrote failing test for viewing a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post saved successfully'
+			redirect_to posts_path
+		else
+      flash[:notice] = 'Post did not save'
+			render 'new'
+		end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
   end
 end
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -14,10 +14,20 @@ feature 'posts' do
     scenario 'lets a user add a post' do
       visit '/posts'
       click_link 'Create a post'
-      fill_in 'text', with: "Miaow miaow"
-      click_link 'Submit'
+      fill_in 'Text', with: "Miaow miaow"
+      click_button 'Create Post'
       expect(current_path).to eq '/posts'
       expect(page).to have_content 'Miaow miaow'
+    end
+  end
+
+  context 'viewing posts' do
+    let!(:kitty){ Post.create(text:'Kitty cat') }
+    scenario 'lets a user view a post' do
+      visit '/posts'
+      click_link 'Kitty cat'
+      expect(page).to have_content 'Kitty cat'
+      expect(current_path).to eq "/posts/#{kitty.id}"
     end
   end
 end

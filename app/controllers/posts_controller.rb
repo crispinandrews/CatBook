c2fd3c3e4339dbc360dfe840aad8b546f5da1028
Frances Maxwell <fmaxwell@gmail.com>
2016-11-23 00:36:52 +0000

    Users are now associated with posts. Styling improved.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at DESC")
+    @user = current_user
   end
 
   def new
@@ -11,9 +12,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @user = current_user
+    @post = @user.posts.new(post_params)
     if @post.save
-      flash[:notice] = 'Post saved successfully'
       redirect_to posts_path
     else
       flash[:notice] = 'Post did not save'
@@ -23,6 +24,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -32,14 +34,13 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    flash[:notice] = 'Post updated successfully'
     redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    flash[:notice] = 'Post deleted successfully'
+    flash[:notice] = 'Post deleted'
     redirect_to posts_path
   end
 
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -4,10 +4,11 @@ feature 'posts' do
 
   before do
     sign_up
+    @user = User.create(email: 'abc@123.com', password: '123456')
   end
 
-  context 'no pictures have been added' do
-    scenario 'should display a prompt to add a picture' do
+  context 'no posts have been added' do
+    scenario 'should display a prompt to add a post' do
       visit '/posts'
       expect(page).to have_content 'No posts'
       expect(page).to have_link 'Create a post'
@@ -18,40 +19,36 @@ feature 'posts' do
     scenario 'lets a user add a text post' do
       add_post('Miaow miaow')
       expect(current_path).to eq '/posts'
-      expect(page).to have_content 'Post saved successfully'
       expect(page).to have_content 'Miaow miaow'
     end
   end
 
   context 'viewing posts' do
-    let!(:kitty){ Post.create(text:'Kitty cat') }
+    let!(:kitty){ @user.posts.create(text:'Kitty cat') }
     scenario 'lets a user view a post' do
-      visit_post('Kitty cat')
-      expect(current_path).to eq "/posts/#{kitty.id}"
+      visit '/posts'
       expect(page).to have_content 'Kitty cat'
     end
   end
 
   context 'editing posts' do
-    before { Post.create(text: 'Kitty cat') }
+    before { @user.posts.create(text: 'Kitty cat') }
     scenario 'lets a user edit a post' do
-      visit_post('Kitty cat')
+      visit '/posts'
       edit_post('Gorgeous cat cat')
       expect(current_path).to eq '/posts'
-      expect(page).to have_content 'Post updated successfully'
       expect(page).to have_content 'Gorgeous cat cat'
       expect(page).not_to have_content 'Kitty cat'
     end
   end
 
   context 'deleting posts' do
-    before { Post.create(text: 'Kitty cat') }
+    before { @user.posts.create(text: 'Kitty cat') }
     scenario 'removes a post when a user clicks delete' do
       visit '/posts'
-      click_link 'Kitty cat'
       click_link 'Delete'
       expect(current_path).to eq '/posts'
-      expect(page).to have_content 'Post deleted successfully'
+      expect(page).to have_content 'Post deleted'
       expect(page).not_to have_content 'Kitty cat'
     end
   end

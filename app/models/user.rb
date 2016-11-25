96b12a7429b97f823d7b89065e902c065debf900
michkles <michkles@gmail.com>
2016-11-25 15:12:39 +0000

    friends will be friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
   has_many :commented_posts, through: :comments, source: :post
   has_many :relationships
-  has_many :relations, through: :relationships, source: :user
+  has_many :relations, through: :relationships, source: :relation
   has_many :inverse_relationships, class_name: "Relationship", foreign_key: "relation_id"
   has_many :inverse_relations, through: :inverse_relationships, source: :user
   devise :database_authenticatable, :registerable,

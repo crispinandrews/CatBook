cbb7203b6cc1a4a06e07ba231faca1e4ece3a24c
Frances Maxwell <fmaxwell@gmail.com>
2016-11-25 16:42:39 +0000

    Having a mare with relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
-  
+
   before_action :authenticate_user!, :except => [:index, :show]
   before_action :require_permisson, only: [:edit, :update, :destroy]
 
 
+# create an array of all the user's relations DONE
+# add in all the user's inverse relations DONE
+# find all the posts from users with those IDs
+
+#Post.all.find post.user == (current_user.relation || current_user.inverse_relation)
+
   def index
+    @posts_from_friends = []
+    # get_friends_posts
     @users = User.all
     @posts = Post.all.order("created_at DESC")
     @user = current_user
@@ -49,6 +57,17 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def get_friends_posts
+    if current_user
+      friends = current_user.relations
+      friends << current_user.inverse_relations
+      # friends << current_user
+      friends.each do |friend|
+        @posts_from_friends << Post.find_by(user_id: friend.id)
+      end
+    end
+  end
 
   def post_params
     params.require(:post).permit(:text, :image)
